04bb9f0fdfa806b42e1e9e676f11395108a0d899
Łukasz Sadowski <lukasz.sadowski2605@gmail.com>
2022-01-20 18:14:53 +0100

    Add new files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const nav = document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navList = document.querySelector('.navbar-collapse')
+
+    const addShadow = () => {
+        if (window.scrollY >= 200){
+            nav.classList.add('shadow-bg')
+        } else {
+            nav.classList.remove('shadow-bg')
+        }
+    }
+
+
+
+    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+
+
+
+
+
+
+    window.addEventListener('scroll', addShadow)
+})
